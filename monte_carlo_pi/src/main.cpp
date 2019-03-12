919dc522a6a015323c76ceab6dfec3d19fe8ec69
andrei.home <vasy1796@gmail.com>
2019-03-12 18:20:13 +0200

    added monte carlo code

--- monte_carlo_pi/src/main.cpp
+++ monte_carlo_pi/src/main.cpp
@@ -2,170 +2,80 @@
 #include <omp.h>
 #include <chrono>
 
-void assign_matrix(int** mat, unsigned int rows,unsigned int cols);
-void assign_vector(int* vec, unsigned int rows,bool is_result_vec);
-void classic_mul(int* vec, int **mat, int* res_vec, unsigned int rows, unsigned int cols);
-void parallel_mul(int* vec, int **mat, int* res_vec, unsigned int rows, unsigned int cols);
-void print_mat(int** mat, unsigned int rows, unsigned int cols);
-void print_vec(int* vec, unsigned int rows);
+float get_random_number(float a, float b);
+float classic_monte_carlo(unsigned int numbers, float a, float b, float radius);
+float parallel_monte_carlo(unsigned int numbers, float a, float b, float radius);
+float man_parallel_monte_carlo(unsigned int numbers, float a, float b, float radius);
 
 int main() 
 {
-    const unsigned int rows = 10'000;
-    const unsigned int cols = rows;
+    const unsigned int numbers = 10'000'000;
 
     //find number of threads fit for rows, should be improved
     const short no_threads = 8;
     omp_set_num_threads(no_threads);
     std::cout << "working on " << no_threads << " threads."<< std::endl;
     
-    //create matrix
-    int** matrix = new int*[rows];
-    for (int i = 0; i < rows; ++i) 
-    {
-        matrix[i] = new int[cols];
-    }
+    const float a = -1;
+    const float b = 1;
+    const float cx = 0.0f;
+    const float cy = 0.0f;
+    const float radius = 1;
 
-    //create vector and result
-    int* vector = new int[rows];
-    int* result = new int[rows];
-
-    //assign values to created matrix/vectors
-    assign_matrix(matrix,rows,cols);
-    assign_vector(vector, rows, false);
-    assign_vector(result, rows, true);
-
-    //begin classic mul
     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-    classic_mul(vector, matrix, result, rows, cols);
+    float pi = classic_monte_carlo(numbers, a, b, radius);
     std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
     std::cout << "Sequential time = " << std::chrono::duration_cast<std::chrono::milliseconds> (end - begin).count() << std::endl;
+    std::cout << "classic pi = " << pi << std::endl;
 
-    //reset result vector
-    assign_vector(result, rows, true);
-
-    //begin parallel mul
     begin = std::chrono::steady_clock::now();
-    parallel_mul(vector, matrix, result, rows, cols);
+    pi = parallel_monte_carlo(numbers, a, b, radius);
     end = std::chrono::steady_clock::now();
     std::cout << "Parallel time = " << std::chrono::duration_cast<std::chrono::milliseconds> (end - begin).count() << std::endl;
-
-    //print only for small sizes
-    if (rows < 10) 
-    {
-        print_mat(matrix, rows, cols);
-        print_vec(vector, rows);
-        print_vec(result, rows);
-    }
-
-    //deallocate memory
-    for (int i = 0; i < rows; ++i) 
-    {
-        delete[] matrix[i];
-    } 
-    delete[] matrix;
-    delete[] vector; 
-    delete[] result;
+    std::cout << "parallel pi = " << pi << std::endl;
 
     return 0;
 }
 
-void assign_matrix(int** mat, unsigned int rows, unsigned int cols)
+float get_random_number(float a, float b) 
 {
-#pragma omp parallel for
-        for (int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < cols; j++)
-            {
-                mat[i][j] = rand() % 100 + i*j;
-            }
-        }
-    //// assign values to allocated matrix, sequential
-    //for (int i = 0; i < rows; i++)
-    //{
-    //    for (int j = 0; j < cols; j++)
-    //    {
-    //        mat[i][j] = rand() % 100;
-    //    }
-
-    //}
+    float random = ((float)rand()) / (float)RAND_MAX;
+    float diff = b - a;
+    float r = random * diff;
+    return a + r;
 }
 
-void assign_vector(int* vec, unsigned int rows, bool is_result_vec)
+float classic_monte_carlo(unsigned int numbers,float a,float b,float radius) 
 {
-#pragma omp parallel for
-        for (int i = 0; i < rows; i++)
-        {
-            if (is_result_vec)
-            {
-                vec[i] = 0;
-            }
-            else
-            {
-                vec[i] = rand() % 100 + i;
-            }
+    float counter = 0;
 
-        }
-    ////assign values to allocated vector, sequential
-    //for (int i = 0; i < rows; i++)
-    //{
-    //    if (is_result_vec) 
-    //    {
-    //        vec[i] = 0;
-    //    }
-    //    else 
-    //    {
-    //        vec[i] = rand() % 100;
-    //    }
-    //    
-    //}
-}
-
-void classic_mul(int* vec, int **mat, int* res_vec, unsigned int rows, unsigned int cols)
-{
-    //perform classic multiplication
-    for (int i = 0; i < rows; i++)
+    for (int i = 0; i < numbers; i++)
     {
-        for (int j = 0; j < cols; j++)
+        float rand_x = get_random_number(a, b);
+        float rand_y = get_random_number(a, b);
+        float distance = sqrt((double)(rand_x - 0.0)*(rand_x - 0.0) + (rand_y - 0.0)*(rand_y - 0.0));
+        if (distance < radius)
         {
-            res_vec[i] += mat[i][j] * vec[j];
+            counter+=1;
         }
     }
+    return 4 * (counter / (float)(numbers));
 }
 
-void parallel_mul(int* vec, int **mat, int* res_vec, unsigned int rows, unsigned int cols)
+float parallel_monte_carlo(unsigned int numbers, float a, float b, float radius) 
 {
-    //multiplication on threads
-#pragma omp parallel for
-        for (int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < cols; j++)
-            {
-                res_vec[i] += mat[i][j] * vec[j];
-            }
-        }
-}
+    float counter = 0;
 
-void print_mat(int** mat, unsigned int rows, unsigned int cols)
-{
-    std::cout << "matrix: " << std::endl;
-    // print the matrix
-    for (int i = 0; i < rows; i++)
+#pragma omp parallel for reduction(+:counter)
+    for (int i = 0; i < numbers; i++)
     {
-        for (int j = 0; j < cols; j++)
+        float rand_x = get_random_number(a, b);
+        float rand_y = get_random_number(a, b);
+        float distance = sqrt((double)(rand_x - 0.0)*(rand_x - 0.0) + (rand_y - 0.0)*(rand_y - 0.0));
+        if (distance < radius)
         {
-            std::cout << mat[i][j] << " ";
+            counter+=1;
         }
-        std::cout << std::endl;
     }
-}
-
-void print_vec(int* vec, unsigned int rows)
-{
-    std::cout << "vector: " << std::endl;
-    // print the vector
-    for (int i = 0; i < rows; i++)
-    {
-        std::cout << vec[i] << std::endl;
-    }
+    return 4 * (counter / (float)(numbers));
 }
