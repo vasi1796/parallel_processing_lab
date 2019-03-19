c1cb871e1d6735edee2a664c6f7946051df976bf
andrei.home <vasy1796@gmail.com>
2019-03-19 10:10:12 +0200

    finished parallel histogram

--- histograma/src/main.cpp
+++ histograma/src/main.cpp
@@ -3,7 +3,7 @@
 #include <utilities.hpp>
 
 void classic_histogram(int imgSize, int *hist,unsigned char *img);
-void parallel_histogram(int imgSize, int *hist, unsigned char *img);
+void parallel_histogram(int imgSize, int *hist, unsigned char *img, int*hist_thread,short no_threads);
 void print_histogram(int *hist);
 
 int main()
@@ -14,12 +14,12 @@ int main()
     // Alocare de memorie pentru histogramă
     int *hist = new int[256];
     // Initializarea imaginii
-    for (int i = 0; i < imgSize; i++)
-        for (int j = 0; j < imgSize; j++)
+    for (int i = 0; i < imgSize; ++i)
+        for (int j = 0; j < imgSize; ++j)
             img[i*imgSize + j] = i % 256;
 
     // Inițializarea histogramei
-    for (int i = 0; i < 256; i++)
+    for (int i = 0; i < 256; ++i)
         hist[i] = 0;
 
     utilities::timeit([&]()
@@ -30,12 +30,21 @@ int main()
     print_histogram(hist);
 
     // Inițializarea histogramei
-    for (int i = 0; i < 256; i++)
+    for (int i = 0; i < 256; ++i)
         hist[i] = 0;
+
+    const short no_threads = 8;
+    omp_set_num_threads(no_threads);
+    std::cout << "working on " << no_threads << " threads." << std::endl;
+    int *hist_thread = new int[256 * no_threads];
+    for (int i = 0; i < 256 * no_threads; ++i)
+    {
+        hist_thread[i] = 0;
+    }
 
     utilities::timeit([&]()
     {
-        parallel_histogram(imgSize, hist, img);
+        parallel_histogram(imgSize, hist, img, hist_thread, no_threads);
     });
 
     print_histogram(hist);
@@ -50,18 +59,31 @@ int main()
 void classic_histogram(int imgSize, int *hist, unsigned char *img)
 {
     // Calcului histogramei
-    for (int i = 0; i < imgSize; i++)
-        for (int j = 0; j < imgSize; j++)
+    for (int i = 0; i < imgSize; ++i)
+        for (int j = 0; j < imgSize; ++j)
             hist[img[i*imgSize + j]]++;
 }
 
-void parallel_histogram(int imgSize, int *hist, unsigned char *img)
+void parallel_histogram(int imgSize, int *hist, unsigned char *img, int*hist_thread, short no_threads)
 {
     // Calcului histogramei
 #pragma omp parallel for
-    for (int i = 0; i < imgSize; i++)
-        for (int j = 0; j < imgSize; j++)
-            hist[img[i*imgSize + j]]++;
+    for (int i = 0; i < imgSize; ++i)
+    {
+        int current_thread = omp_get_thread_num();
+        for (int j = 0; j < imgSize; ++j) 
+        {
+            hist_thread[current_thread * 256 + img[i * imgSize + j]]++;
+        }
+    }
+    
+    for (int i = 0; i < 256; ++i) 
+    {
+        for (int j = 0; j < no_threads; ++j) 
+        {
+            hist[i] += hist_thread[j * 256 + i];
+        }
+    }
 }
 
 void print_histogram(int *hist) 
