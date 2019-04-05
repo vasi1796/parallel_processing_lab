8fb43be485d9aa61e5d283dd52ffb27e6a027672
andrei.home <vasy1796@gmail.com>
2019-04-05 12:39:25 +0300

    custom values for median filter added in cmd

--- cuda_filter/src/main.cu
+++ cuda_filter/src/main.cu
@@ -7,27 +7,43 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
 
-__global__ void median_filter(float *a,float *b, int N, int M) 
+__global__ void median_filter(float *a,float *b, int N, int M, int win_size) 
 {
     int row = blockIdx.x * blockDim.x + threadIdx.x;
     int col = blockIdx.y * blockDim.y + threadIdx.y;
-    if (row < (N - 1) && col < (M - 1) && row > 0 && col > 0) 
+    int factor = win_size / 2;
+    float elems = win_size * win_size;
+    if (row < (N - factor) && col < (M - factor) && row >= factor && col >= factor)
     {
         float sum = 0.f;
-        for (int i = row - 1; i < row + 1; ++i) 
+        for (int i = row - factor; i <= row + factor; ++i)
         {
-            for (int j = col - 1; j < col + 1; ++j)
+            for (int j = col - factor; j <= col + factor; ++j)
             {
                 sum += a[i*N + j];
             }
         }
-        b[row * N + col] = sum/9.f;
+        b[row * N + col] = sum/ elems;
     }
 }
 
 // main routine that executes on the host
-int main()
+int main(int argc, char* argv[])
 {
+    int filter_size;
+    if (argc > 1) 
+    {
+        int size = atoi(argv[1]);
+        if (size % 2) 
+        {
+            filter_size = size;
+        }
+    }
+    else 
+    {
+        filter_size = 3;
+    }
+    
     float *a_h, *a_d, *b_d;
     cv::Mat input_img,filtered_img;
     input_img = cv::imread("D:/dev/Programming/School/PPD/res/ex_sp.png", cv::IMREAD_GRAYSCALE);
@@ -62,7 +78,7 @@ int main()
     dim3 grid(grid_x,grid_y,1);
     dim3 thread(thread_y, thread_y,1);
 
-    median_filter <<<grid, thread>>> (a_d,b_d,input_img.rows,input_img.cols);
+    median_filter <<<grid, thread>>> (a_d,b_d,input_img.rows,input_img.cols,filter_size);
 
     //copiere data pe host
     cudaMemcpy(a_h, b_d, size, cudaMemcpyDeviceToHost);
@@ -77,6 +93,9 @@ int main()
 
     filtered_img.convertTo(filtered_img, CV_8UC1);
     cv::imshow("image filtered", filtered_img);
+    std::stringstream sstream;
+    sstream << "filter_" << filter_size << ".jpg";
+    cv::imwrite(sstream.str(), filtered_img);
     cv::waitKey(0);
 
     //cuda cleanup
