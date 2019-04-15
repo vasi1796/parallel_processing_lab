6a02ee1f21a6fd7fa836b6ddb9f929be947e2804
andrei.home <vasy1796@gmail.com>
2019-04-15 18:58:48 +0300

    removed unused code

--- cuda_reduction/src/main.cu
+++ cuda_reduction/src/main.cu
@@ -4,14 +4,10 @@
 #include <device_launch_parameters.h>
 #include <utilities.hpp>
 
-__global__ void cuda_max(float* a_d, float* out, int size)
+__global__ void cuda_max(float* a_d, float* out)
 {
     __shared__ float a_sh[128];
     const unsigned int index = blockIdx.x*blockDim.x + threadIdx.x;
-    if (index >= size)
-    {
-        return;
-    }
     a_sh[threadIdx.x] = a_d[index];
     __syncthreads();
     for (unsigned int s = blockDim.x >> 1; s > 0; s >>= 1)
@@ -33,66 +29,61 @@ int main()
     float *a_h, *a_d, *b_h, *b_d;
     b_d = NULL;
     int N = 1024*1024;
-    size_t size = N * sizeof(float);
+    size_t size_of_vector = N * sizeof(float);
 
-    a_h = (float*)malloc(size);
-    b_h = (float*)malloc(size);
-    cudaMalloc((void**)&a_d, size);
-    cudaMalloc((void**)&b_d, size);
+    a_h = (float*)malloc(size_of_vector);
+    cudaMalloc((void**)&a_d, size_of_vector);
 
-    //dimensiuni grid si threads
-    const int thread = 128;
-
-    const int grid = N / thread;
+    const int grid = N / 128;
 
     dim3 grids(grid, 1, 1);
-    dim3 threads(thread, 1, 1);
+    dim3 threads(128, 1, 1);
 
+    int rand_end = 1;
     for (int i = 0; i < N; ++i)
     {
-        a_h[i] = 1 + (rand() % (i + 1));
-    }
-    float max = -1;
-
-    for (int i = 0; i < N; ++i)
-    {
-        if (a_h[i] > max)
+        if (rand_end == 255) 
         {
-            max = a_h[i];
+            rand_end = 0;
+        }
+        rand_end++;
+        a_h[i] = 1 + (rand() % rand_end);
+    }
+    float max_h = -1;
+
+    for (int i = 0; i < N; ++i)
+    {
+        if (a_h[i] > max_h)
+        {
+            max_h = a_h[i];
         }
     }
 
-    cudaMemcpy(a_d, a_h, size, cudaMemcpyHostToDevice);
-    while(N>grids.x)
+    cudaMemcpy(a_d, a_h, size_of_vector, cudaMemcpyHostToDevice);
+    while(N > threads.x)
     {
         cudaMalloc((void**)&b_d, N / 128 * sizeof(float));
         dim3 grids(N / 128);
-        cuda_max << <grids, threads >> > (a_d, b_d, N);
+        cuda_max << <grids, threads >> > (a_d, b_d);
         cudaFree(a_d);
         a_d = b_d;
-        //cudaMemcpy(a_d, b_d, N*sizeof(float), cudaMemcpyDeviceToDevice);
         N /= 128;
     }
-    float *c_h;
-    c_h = (float*)malloc(N*sizeof(float));
-    cudaMemcpy(c_h, a_d, N * sizeof(float), cudaMemcpyDeviceToHost);
-    float maxim = -1;
+    b_h = (float*)malloc(N*sizeof(float));
+    cudaMemcpy(b_h, a_d, N * sizeof(float), cudaMemcpyDeviceToHost);
+    float max_d = -1;
     for (int i = 0; i < N; ++i) 
     {
-        if (c_h[i] > maxim)
+        if (b_h[i] > max_d)
         {
-            maxim = c_h[i];
+            max_d = b_h[i];
         }
     }
-    /*cuda_max << <grids, threads >> > (a_d, b_d, N);
-    cudaMemcpy(b_h, b_d, size, cudaMemcpyDeviceToHost);*/
-    std::cout << "cpu max " << max << std::endl;
-    std::cout << "gpu max " << maxim << std::endl;
+    std::cout << "cpu max " << max_h << std::endl;
+    std::cout << "gpu max " << max_d << std::endl;
     
     cudaFree(a_d);
-    //cudaFree(b_d);
     free(a_h);
     free(b_h);
-    free(c_h);
     return 0;
 }
